75bb26cabcade53c1603fe5a4f1bd6dd03535d79
Vicky Shinde <vicky.shinde@weboniselab.com>
2024-07-16 16:35:37 +0530

    add a book functionality

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,11 +1,29 @@
-import React from 'react';
+// src/BookList.js
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { deleteBook } from '../rtk/booksSlice';
+import { addBook, deleteBook } from '../rtk/booksSlice';
 
 const BookList = () => {
   const books = useSelector((state) => state.books);
   const dispatch = useDispatch();
+  const [newBook, setNewBook] = useState({ name: '', summary: '' });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewBook({ ...newBook, [name]: value });
+  };
+
+  const handleAddBook = (e) => {
+    e.preventDefault();
+    if (newBook.name && newBook.summary) {
+      const newBookWithId = { ...newBook, id: books.length + 1 };
+      dispatch(addBook(newBookWithId));
+      setNewBook({ name: '', summary: '' });
+    } else {
+      alert('Please fill in both fields');
+    }
+  };
 
   const handleDelete = (id) => {
     dispatch(deleteBook(id));
@@ -22,6 +40,23 @@ const BookList = () => {
           </li>
         ))}
       </ul>
+      <form onSubmit={handleAddBook}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Book Name"
+          value={newBook.name}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="summary"
+          placeholder="Book Summary"
+          value={newBook.summary}
+          onChange={handleChange}
+        />
+        <button type="submit">Add Book</button>
+      </form>
       <button onClick={() => window.history.back()}>Back</button>
     </div>
   );
